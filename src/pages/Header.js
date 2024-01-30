ba9c749d39040f659228a10f0819b2fa91f95f55
hashcookie <hashcookie404@gmail.com>
2024-01-30 22:42:12 +0800

    Add menu toggle functionality to Header component

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="border-b py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px]">
       <div className="flex flex-wrap items-center gap-x-2 max-lg:gap-y-6">
         <a href="#!">
-          <img
-            src="https://readymadeui.com/readymadeui.svg"
-            alt="logo"
-            className="w-36"
-          />
+          <img src="/CET.svg" alt="logo" className="w-12" />
         </a>
-        <button id="toggle" className="lg:hidden ml-auto">
+        <button id="toggle" className="lg:hidden ml-auto" onClick={toggleMenu}>
           <svg
             className="w-7 h-7"
             fill="#000"
@@ -20,47 +22,49 @@ const Header = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
+              clipRule="evenodd"
+            />
           </svg>
         </button>
         <ul
           id="collapseMenu"
-          className="lg:!flex lg:ml-14 lg:space-x-5 max-lg:space-y-2 max-lg:hidden max-lg:py-4 max-lg:w-full"
+          className={`lg:flex lg:ml-14 lg:space-x-5 ${
+            isMenuOpen ? "flex" : "hidden"
+          } flex-col lg:flex-row lg:items-center max-lg:py-4 max-lg:w-full`}
         >
           <li className="max-lg:border-b max-lg:py-2 px-3">
             <Link
               to="/cet4"
-              classNameName="lg:hover:text-[#007bff] text-[#007bff] block font-semibold text-[15px]"
+              className="text-[#007bff] block font-semibold text-[15px]"
             >
               CET4
             </Link>
           </li>
           <li className="max-lg:border-b max-lg:py-2 px-3">
-            <a
-              href="#!"
-              className="lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+            <Link
+              to="/cet6"
+              className="text-gray-500 block font-semibold text-[15px]"
             >
               CET6
-            </a>
+            </Link>
           </li>
           <li className="max-lg:border-b max-lg:py-2 px-3">
-            <a
-              href="#!"
-              className="lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+            <Link
+              to="/practice"
+              className="text-gray-500 block font-semibold text-[15px]"
             >
               专项练习
-            </a>
+            </Link>
           </li>
           <li className="max-lg:border-b max-lg:py-2 px-3">
-            <a
-              href="#!"
-              className="lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]"
+            <Link
+              to="/feedback"
+              className="text-gray-500 block font-semibold text-[15px]"
             >
               反馈
-            </a>
+            </Link>
           </li>
         </ul>
         <div className="flex lg:ml-auto max-lg:w-full">
