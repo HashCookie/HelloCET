668e367ae83eed38e1b4653440714c0a108aaf9a
hashcookie <hashcookie404@gmail.com>
2024-03-15 16:04:19 +0800

    style: Update styles in Writing component

--- src/components/Writing/Writing.tsx
+++ src/components/Writing/Writing.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import styles from "../../styles/Writing.module.css";
 
 interface WritingTestPageProps {
   basePath: string;
@@ -26,7 +25,7 @@ const WritingTestPage: React.FC<WritingTestPageProps> = ({ basePath }) => {
   };
 
   return (
-    <div className={styles["writing-test-container"]}>
+    <div className="w-4/5 mx-auto p-5">
       <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-center mb-6">
         Part1 Writing
       </h1>
@@ -34,18 +33,18 @@ const WritingTestPage: React.FC<WritingTestPageProps> = ({ basePath }) => {
         dangerouslySetInnerHTML={{
           __html: Directions.replace(/(Directions:)/, "<strong>$1</strong>"),
         }}
-        className="font-normal"
+        className="text-base mb-5"
       ></p>
       <textarea
         value={essay}
         onChange={handleInputChange}
-        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-40 mb-2.5"
         placeholder="Please enter text."
         maxLength={180}
       />
       <button
         onClick={() => console.log(essay)}
-        className="px-6 py-2.5 rounded-full text-white text-sm tracking-wider font-semibold border-none outline-none bg-blue-600 hover:bg-blue-700 active:bg-blue-600"
+        className="py-2.5 px-5 rounded-full text-white text-sm tracking-wider font-semibold border-none outline-none bg-blue-600 hover:bg-blue-700 active:bg-blue-600 block mx-auto"
       >
         提交
       </button>
