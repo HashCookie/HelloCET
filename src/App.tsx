0a98c0a20c30b08e611452d5ddfa186d1b825edb
hashcookie <hashcookie404@gmail.com>
2024-04-09 16:30:25 +0800

    style: Remove unnecessary imports and CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./pages/Header";
 import YearAndSetSelector from "./components/YearAndSetSelector";
--- src/pages/ScoresHistory.tsx
+++ src/pages/ScoresHistory.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Table } from "@radix-ui/themes";
 
 // 定义记录的类型
