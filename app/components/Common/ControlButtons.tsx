1611df0c7f0e8a309ee9b3085752759a6e6a6919
Coki <hashcookie404@gmail.com>
2024-10-31 02:34:49 +0800

    feat: add routing functionality to ControlButtons component

--- app/components/Common/ControlButtons.tsx
+++ app/components/Common/ControlButtons.tsx
@@ -1,14 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 interface ControlButtonsProps {
   onReset: () => void;
 }
 
 const ControlButtons = ({ onReset }: ControlButtonsProps) => {
+  const router = useRouter();
+
   return (
     <div className="flex justify-end space-x-4 pr-4">
       <button className="blue-button mt-4" onClick={onReset}>
         重新选择
       </button>
-      <button className="blue-button mt-4">返回</button>
+      <button className="blue-button mt-4" onClick={() => router.push("/")}>
+        返回
+      </button>
     </div>
   );
 };
